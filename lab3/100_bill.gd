c543f3dfa849290353b7235cd78d1c95b074d099
Anton M <antonmaironi@gmail.com>
2025-02-25 23:07:33 -0500

    Updated bill behavior

--- lab3/100_bill.gd
+++ lab3/100_bill.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	queue_free()
-	
+	score += 100
--- lab3/25_bill.gd
+++ lab3/25_bill.gd
@@ -1,11 +1,15 @@
-extends Node2D
+extends RigidBody2D
 
 var score = 0
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print(score)
+func _process(delta):
+	pass
+
+
+func _on_area_2d_area_entered(area):
+	queue_free()
+	score += 25
