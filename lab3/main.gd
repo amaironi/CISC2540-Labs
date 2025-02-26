d05cbc404501c712971812c61c20393325c9d424
Anton M <antonmaironi@gmail.com>
2025-02-26 00:03:16 -0500

    Added win condition
    Need a score of 400 out of 525 (500 possible)

--- lab3/main.gd
+++ lab3/main.gd
@@ -4,15 +4,15 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var label = $Label
-	label.text = "Wealth: " + str(score)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var label = $Label
-	label.text = "Wealth: " + str(score)
-
+	label.text = "Wealth (need at least 400 to win): " + str(score) + "/500"
+	if score >= 400:
+		get_tree().paused = true
 
 func _on_bill_score_increase_100(amount: Variant) -> void:
 	score+=100 # Replace with function body.
